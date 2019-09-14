557cc7f7fafaa83a484e2ac422d771b2305d376d
yanxianUCSB <yanxian@umail.ucsb.edu>
2019-09-14 15:30:47 -0700

    TST: Add testcase for nmr.py nmr.det_type

--- Tester.py
+++ Tester.py
@@ -1,30 +1,48 @@
 import unittest
 import returnIntegralsDev as retint
 from returnIntegralsDev import workupODNP
+from os import getcwd
+from pyspecdata.nmr import *
+
+class TestNMR(unittest.TestCase):
+    def setUp(self):
+        self.filename = getcwd() + "/test/in/180329_HttQ25_30R1_22C_ODNP_3/1"
+
+    def tearDown(self):
+        pass
+
+    def test_det_type(self):
+        filetype, twod = det_type(self.filename)
+        self.assertEqual(filetype, 'bruker')
+        self.assertFalse(twod)
 
 
 class TestWorkupODNP(unittest.TestCase):
 
     def setUp(self):
+        cwd = getcwd()
         self.wo = workupODNP()
-        self.wo.DataDir = "/Users/yanxlin/github/workupsoftware/test/in"
-        self.wo.ODNPFile= "/Users/yanxlin/github/workupsoftware/test/in/" \
-                          "180329_HttQ25_30R1_22C_ODNP_3"
-        pass
+        self.wo.DataDir = cwd + "/test/in"
+        self.wo.ODNPFile= cwd + "/test/in/180329_HttQ25_30R1_22C_ODNP_3"
 
     def tearDown(self):
         pass
 
-    def test_determine_exp_type(self):
+    def disabled_test_determine_exp_type(self):
         self.wo.determine_exp_type()
         self.assertEqual(self.wo.setType, 'dnpExp')
         with self.assertRaises(ValueError):
             workupODNP().determine_exp_type()
-        pass
 
     def test_resturn_exp_numbers(self):
-        if self.wo.isNMRExp:
-            self.wo.returnExpNumbers()
+        self.wo.determine_exp_type()
+        exp_titles, dnp_exp_nums, t1_exp_nums = self.wo.return_exp_numbers()
+        self.assertNotEqual(dnp_exp_nums, [])
+        # self.assertNotEqual(t1_exp_nums, [])
+        # self.assertEqual(exp_titles, "1", msg=exp_titles[0])
+        #
+        # self.assertEqual(dnp_exp_nums, "1", msg=dnp_exp_nums)
+        print(exp_titles, dnp_exp_nums, t1_exp_nums)
 
 
 if __name__ == '__main__':
